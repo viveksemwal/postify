b9b4a717700443311c770fb3a6af8b4eecb6244b
Vivek Semwal <viveksemwal@localhost.localdomain>
2021-03-26 21:02:00 +0530

    my first commit

---
+++ src/main/java/com/vivek/postify/PostifyApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.postify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostifyApplication.class, args);
+	}
+
+}
